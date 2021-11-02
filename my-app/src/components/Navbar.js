af95eee5678de94697cffad4700574ad22c03668
김건호 <gunho7084@gmail.com>
2021-11-02 14:35:06 +0900

    commit

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,11 +4,33 @@ import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
+import "./Header.css";
 import { IconContext } from "react-icons";
+import axios from "axios";
+
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+  //const pageRe = () => {};
+  const handleChagne = (e) => {
+    if (e.keyCode == 13) {
+      // 엔터키가 눌리면
+      const textbox = {
+        inText: e.target.value, // 검색창에 입력된 값 저장
+      };
+      fetch("http://localhost:5000/text", {
+        // 크롤링 서버로 값 전송
+        method: "post", //통신방법
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(textbox), //textbox라는 객체를 보냄
+      });
+      e.target.value = "";
+      document.location.href = "/products";
+    }
+  };
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
@@ -16,6 +38,29 @@ function Navbar() {
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
+          <div className="contents">
+            <div>로고 자리</div>
+            <div style={{ height: "60px" }}></div>
+            <table className={"table"}>
+              <tr>
+                <td>
+                  <input
+                    type={"text"}
+                    className={"input-sm"}
+                    size={"100"}
+                    height={"50"}
+                    onKeyDown={handleChagne}
+                  />
+                </td>
+              </tr>
+            </table>
+            <nav className="navigation">
+              <ul>
+                <li>[최근 본 상품] </li>
+                <li>[로그인]</li>
+              </ul>
+            </nav>
+          </div>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
