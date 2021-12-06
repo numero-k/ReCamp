78c936048dafd9431708dfbdc5fa09e3b753bcae
c970112 <c970112@naver.com>
2021-12-06 18:55:55 +0900

    수정1

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,23 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const db = require("./config/db");
 
-app.get("/api/host", (req, res) => {
-  res.send({ host: "seon" });
-});
-
-app.get("/api/test", (req, res) => {
-  db.query("select * from Test", (err, data) => {
-    if (!err) {
-      res.send(data);
-    } else {
-      console.log(err);
-      res.send(err);
-    }
-  });
-});
+app.get('/', (req, res) => {
+  res.send(`Response Complate`);
+})
 
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
-});
+})
